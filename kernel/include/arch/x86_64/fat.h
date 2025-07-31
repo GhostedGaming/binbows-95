964355939b5c3bb65c2f96c2ba24196c1574a85c
GhostedGaming <epicrobloxgamer41@gmail.com>
2025-07-30 22:35:06 -0400

    fat

--- kernel/include/arch/x86_64/fat.h
+++ kernel/include/arch/x86_64/fat.h
@@ -3,152 +3,63 @@
 
 #include <stdint.h>
 
-// FAT32 Boot Sector structure
-struct __attribute__((packed)) fat32_boot_sector {
-    uint8_t jump[3];               // Jump instruction
-    uint8_t oem_name[8];           // OEM name
-    uint16_t bytes_per_sector;     // Bytes per sector
-    uint8_t sectors_per_cluster;   // Sectors per cluster
-    uint16_t reserved_sectors;     // Reserved sectors
-    uint8_t num_fats;              // Number of FATs
-    uint16_t root_entries;         // Root directory entries (0 for FAT32)
-    uint16_t total_sectors_16;     // Total sectors (0 for FAT32)
-    uint8_t media_descriptor;      // Media descriptor
-    uint16_t sectors_per_fat_16;   // Sectors per FAT (0 for FAT32)
-    uint16_t sectors_per_track;    // Sectors per track
-    uint16_t num_heads;            // Number of heads
-    uint32_t hidden_sectors;       // Hidden sectors
-    uint32_t total_sectors_32;     // Total sectors
-    uint32_t sectors_per_fat_32;   // Sectors per FAT
-    uint16_t flags;                // Flags
-    uint16_t version;              // Version
-    uint32_t root_cluster;         // Root directory cluster
-    uint16_t fsinfo_sector;        // FSInfo sector
-    uint16_t backup_boot_sector;   // Backup boot sector
-    uint8_t reserved[12];          // Reserved
-    uint8_t drive_number;          // Drive number
-    uint8_t reserved1;             // Reserved
-    uint8_t boot_signature;        // Boot signature
-    uint32_t volume_serial;        // Volume serial number
-    uint8_t volume_label[11];      // Volume label
-    uint8_t fs_type[8];            // File system type
-    uint8_t boot_code[420];        // Boot code
-    uint16_t boot_sector_signature; // Boot sector signature (0xAA55)
-};
+#define SECTOR_SIZE 512
 
-// FAT32 FSInfo structure
-struct __attribute__((packed)) fat32_fsinfo {
-    uint32_t lead_signature;       // Lead signature (0x41615252)
-    uint8_t reserved1[480];        // Reserved
-    uint32_t struct_signature;     // Structure signature (0x61417272)
-    uint32_t free_count;           // Free cluster count
-    uint32_t next_free;            // Next free cluster
-    uint8_t reserved2[12];         // Reserved
-    uint32_t trail_signature;      // Trail signature (0xAA550000)
-};
+typedef struct __attribute__((packed)) {
+    uint8_t  jmpBoot[3];
+    uint8_t  OEMName[8];
+    uint16_t bytesPerSector;
+    uint8_t  sectorsPerCluster;
+    uint16_t reservedSectorCount;
+    uint8_t  numFATs;
+    uint16_t rootEntryCount;
+    uint16_t totalSectors16;
+    uint8_t  media;
+    uint16_t FATSize16;
+    uint16_t sectorsPerTrack;
+    uint16_t numHeads;
+    uint32_t hiddenSectors;
+    uint32_t totalSectors32;
+    uint8_t  driveNumber;
+    uint8_t  reserved1;
+    uint8_t  bootSignature;
+    uint32_t volumeID;
+    uint8_t  volumeLabel[11];
+    uint8_t  fileSystemType[8];
+} FAT16_BPB;
 
-// FAT32 Directory Entry structure
-struct __attribute__((packed)) fat32_dir_entry {
-    uint8_t name[11];              // File name (8.3 format)
-    uint8_t attributes;            // File attributes
-    uint8_t reserved;              // Reserved
-    uint8_t creation_time_tenth;   // Creation time (tenths of second)
-    uint16_t creation_time;        // Creation time
-    uint16_t creation_date;        // Creation date
-    uint16_t last_access_date;     // Last access date
-    uint16_t cluster_high;         // High 16 bits of cluster number
-    uint16_t last_write_time;      // Last write time
-    uint16_t last_write_date;      // Last write date
-    uint16_t cluster_low;          // Low 16 bits of cluster number
-    uint32_t file_size;            // File size
-};
+typedef struct __attribute__((packed)) {
+    uint8_t  name[8];
+    uint8_t  ext[3];
+    uint8_t  attr;
+    uint8_t  reserved;
+    uint8_t  creationTimeTenth;
+    uint16_t creationTime;
+    uint16_t creationDate;
+    uint16_t lastAccessDate;
+    uint16_t firstClusterHigh;
+    uint16_t writeTime;
+    uint16_t writeDate;
+    uint16_t firstClusterLow;
+    uint32_t fileSize;
+} FAT16_DirectoryEntry;
 
-// Long File Name (LFN) entry structure
-struct __attribute__((packed)) fat32_lfn_entry {
-    uint8_t order;                 // Order of this entry in sequence
-    uint16_t name1[5];             // First 5 characters (UTF-16)
-    uint8_t attributes;            // Attributes (always 0x0F for LFN)
-    uint8_t type;                  // Type (always 0 for LFN)
-    uint8_t checksum;              // Checksum of 8.3 name
-    uint16_t name2[6];             // Next 6 characters (UTF-16)
-    uint16_t first_cluster;        // First cluster (always 0 for LFN)
-    uint16_t name3[2];             // Last 2 characters (UTF-16)
-};
+#define FAT16_FREE_CLUSTER     0x0000
+#define FAT16_BAD_CLUSTER      0xFFF7
+#define FAT16_EOC_MARKER       0xFFF8
 
-// FAT32 File System Information structure
-struct fat32_fs {
-    struct fat32_boot_sector *boot_sector;
-    struct fat32_fsinfo *fsinfo;
-    uint32_t *fat_table;           // Cached FAT table
-    uint32_t bytes_per_sector;
-    uint32_t sectors_per_cluster;
-    uint32_t bytes_per_cluster;
-    uint32_t reserved_sectors;
-    uint32_t num_fats;
-    uint32_t sectors_per_fat;
-    uint32_t root_cluster;
-    uint32_t fat_start_sector;
-    uint32_t data_start_sector;
-    uint32_t total_clusters;
-    uint32_t free_clusters;        // Cached free cluster count
-    uint32_t next_free_cluster;    // Next free cluster hint
-};
+#define ATTR_READ_ONLY   0x01
+#define ATTR_HIDDEN      0x02
+#define ATTR_SYSTEM      0x04
+#define ATTR_VOLUME_ID   0x08
+#define ATTR_DIRECTORY   0x10
+#define ATTR_ARCHIVE     0x20
+#define ATTR_LONG_NAME   (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID)
 
-// File handle structure
-struct fat32_file {
-    struct fat32_fs *fs;
-    uint32_t first_cluster;
-    uint32_t current_cluster;
-    uint32_t file_size;
-    uint32_t position;
-    uint8_t attributes;
-    char name[256];                // Long filename support
-};
+int fat16_init(void);
+int fat16_format(uint32_t total_sectors);
 
-// File attribute flags
-#define FAT_ATTR_READ_ONLY  0x01
-#define FAT_ATTR_HIDDEN     0x02
-#define FAT_ATTR_SYSTEM     0x04
-#define FAT_ATTR_VOLUME_ID  0x08
-#define FAT_ATTR_DIRECTORY  0x10
-#define FAT_ATTR_ARCHIVE    0x20
-#define FAT_ATTR_LONG_NAME  0x0F
-
-// FAT32 cluster markers
-#define FAT32_END_OF_CHAIN  0x0FFFFFFF
-#define FAT32_BAD_CLUSTER   0x0FFFFFF7
-#define FAT32_FREE_CLUSTER  0x00000000
-
-// FAT32 signatures
-#define FAT32_BOOT_SIGNATURE        0xAA55
-#define FAT32_FSINFO_LEAD_SIG       0x41615252
-#define FAT32_FSINFO_STRUCT_SIG     0x61417272
-#define FAT32_FSINFO_TRAIL_SIG      0xAA550000
-
-// LFN constants
-#define LFN_LAST_ENTRY      0x40
-#define LFN_DELETED         0xE5
-
-// Error codes
-#define FAT32_SUCCESS       0
-#define FAT32_ERROR         -1
-#define FAT32_NOT_FOUND     -2
-#define FAT32_NO_SPACE      -3
-#define FAT32_INVALID       -4
-
-// Function declarations
-int fat32_init(struct fat32_fs *fs, void *boot_sector);
-uint32_t fat32_get_next_cluster(struct fat32_fs *fs, uint32_t cluster);
-int fat32_read_cluster(struct fat32_fs *fs, uint32_t cluster, void *buffer);
-int fat32_write_cluster(struct fat32_fs *fs, uint32_t cluster, void *buffer);
-struct fat32_dir_entry *fat32_find_file(struct fat32_fs *fs, const char *filename);
-int fat32_open_file(struct fat32_fs *fs, const char *filename, struct fat32_file *file);
-int fat32_read_file(struct fat32_file *file, void *buffer, uint32_t size);
-int fat32_write_file(struct fat32_file *file, void *buffer, uint32_t size);
-int fat32_close_file(struct fat32_file *file);
-uint32_t fat32_allocate_cluster(struct fat32_fs *fs);
-int fat32_free_cluster_chain(struct fat32_fs *fs, uint32_t cluster);
-int fat32_create_file(struct fat32_fs *fs, const char *filename);
-int fat32_delete_file(struct fat32_fs *fs, const char *filename);
+int fat16_find_file(const char* name, FAT16_DirectoryEntry* outEntry);
+int fat16_read_file(FAT16_DirectoryEntry* fileEntry, uint8_t* buffer, uint32_t bufferSize);
 
 #endif // FAT_H
--- kernel/src/kernel/main.c
+++ kernel/src/kernel/main.c
@@ -12,115 +12,113 @@
 #include <acpi.h>
 #include <memory.h>
 #include <ide.h>
-// If you dont know what #include does GETOUT!
+#include <fat.h>
 
 extern volatile struct limine_memmap_request memmap_request;
 extern volatile struct limine_hhdm_request hhdm_request;
 extern volatile struct limine_bootloader_info_request bootloader_request;
 
+extern struct ide_device ide_devices[4];
+
 void kernel_main(void) {
-    // Setup serial
     init_serial();
-    
-    // Initialize GDT
+
     gdt_init();
     gdt_load();
     serial_printf("GDT loaded\n");
-    
-    // Setup the IDT with all its interrupts
+
     idt_init();
     install_exceptions();
     init_timer_irq();
     init_keyboard_irq();
     idt_load();
     init_pic();
-    
-    // Initialize PIT
+
     init_timer();
     asm volatile ("sti");
-    
-    // Check if memory map is available
+
     if (memmap_request.response == NULL) {
         serial_printf("memmap_request.response is NULL!\n");
-        while(1) asm volatile ("hlt");
+        while (1) asm volatile ("hlt");
     }
-    
-    // Print memory map for debugging
+
     serial_printf("Memory map entries:\n");
 
-    
-    // Find the largest usable memory region for buddy allocator
     uintptr_t usable_phys_base = 0;
     size_t usable_length = 0;
-    
+
     for (size_t i = 0; i < memmap_request.response->entry_count; i++) {
         struct limine_memmap_entry* entry = memmap_request.response->entries[i];
-        
-        // Look for USABLE memory (type 0), not reserved (type 1)!
         if (entry->type == 0 && entry->length > usable_length) {
             usable_phys_base = (uintptr_t)entry->base;
             usable_length = (size_t)entry->length;
         }
     }
-    
+
     if (usable_phys_base == 0) {
         serial_printf("No usable memory found!\n");
         while (1) asm volatile ("hlt");
     }
-    
-    // Check HHDM
+
     if (hhdm_request.response == NULL) {
         serial_printf("hhdm_request.response is NULL!\n");
         while (1) asm volatile ("hlt");
     }
-    
+
     uintptr_t hhdm_offset = (uintptr_t)hhdm_request.response->offset;
     void* usable_virt_base = (void*)(usable_phys_base + hhdm_offset);
-    
+
     serial_printf("Using usable memory base (phys)=0x%lx (virt)=0x%lx length=0x%lx\n",
-                 (unsigned long)usable_phys_base,
-                 (unsigned long)(uintptr_t)usable_virt_base,
-                 (unsigned long)usable_length);
-    
-    // Reserve more memory for early page table allocation
-    // Conservative approach: map only essential regions
-    size_t early_alloc_size = 0x800000; // 8MB should be enough for essential mappings
+                  (unsigned long)usable_phys_base,
+                  (unsigned long)(uintptr_t)usable_virt_base,
+                  (unsigned long)usable_length);
+
+    size_t early_alloc_size = 0x800000; // 8MB
     void* buddy_base = (void*)((uintptr_t)usable_virt_base + early_alloc_size);
     size_t buddy_size = usable_length - early_alloc_size;
-    
-    // Initialize paging first (uses early allocator)
+
     paging_init(usable_virt_base, early_alloc_size);
     serial_printf("Paging initialized\n");
-    
-    // Now initialize buddy allocator with remaining memory
+
     buddy_init(buddy_base, buddy_size);
     serial_printf("Buddy allocator initialized\n");
 
     if (acpi_init() != 0) {
-        write_serial("ACPI Failed");
+        write_serial("ACPI initialization failed\n");
     }
 
     ide_initialize();
+
     if (bootloader_request.response) {
         serial_printf("Bootloader: %s %s\n",
-            bootloader_request.response->name,
-            bootloader_request.response->version);
+                      bootloader_request.response->name,
+                      bootloader_request.response->version);
     }
-    
-    serial_printf("Testing read/write!\n");
-    uint8_t sector[512] = { 'H', 'e', 'l', 'l', 'o', '!', 0 };
-    ide_write_sectors(0, 1, 1, sector);
 
-    int err = ide_read_sectors(0, 1, 1, sector);
-    if (err) {
-        serial_printf("Failed to read boot sector!\n");
+    uint32_t total_sectors = ide_devices[0].Size;
+    serial_printf("Formatting drive 0 (%u sectors) as FAT16...\n", total_sectors);
+
+    int res = fat16_format(total_sectors);
+    if (res != 0) {
+        serial_printf("Failed to format drive 0 as FAT16! Error code: %d\n", res);
+        while (1) asm volatile ("hlt");
     } else {
-        for (int i = 0; i < 64; i++) {
-            serial_printf("%02X ", sector[i]);
-            if ((i + 1) % 16 == 0) serial_printf("\n");
+        serial_printf("Drive 0 formatted successfully as FAT16.\n");
+    }
+
+    uint8_t sector[512] = { 'H', 'e', 'l', 'l', 'o', '!', 0 };
+    if (ide_write_sectors(0, 1, 1, sector) != 0) {
+        serial_printf("Failed to write to sector 1!\n");
+    } else {
+        if (ide_read_sectors(0, 1, 1, sector) != 0) {
+            serial_printf("Failed to read sector 1!\n");
+        } else {
+            for (int i = 0; i < 64; i++) {
+                serial_printf("%02X ", sector[i]);
+                if ((i + 1) % 16 == 0) serial_printf("\n");
+            }
         }
     }
-    serial_printf("Read/Write works\n");
 
     while (1) asm volatile ("hlt");
 }
