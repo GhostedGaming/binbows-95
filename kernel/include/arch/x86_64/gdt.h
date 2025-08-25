f96f33165c895277082db715c06f1c154c869a82
GhostedGaming <epicrobloxgamer41@gmail.com>
2025-08-25 15:31:51 +0000

    made a new gdt

--- kernel/include/arch/x86_64/gdt.h
+++ kernel/include/arch/x86_64/gdt.h
@@ -1,19 +1,20 @@
 #ifndef GDT_H
 #define GDT_H
 
-// GDT entry structure
 struct gdt_entry {
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_middle;
-    uint8_t access;
-    uint8_t granularity;
-    uint8_t base_high;
+	/* Limits */
+	unsigned short limit_low;
+	/* Segment address */
+	unsigned short base_low;
+	unsigned char base_middle;
+	/* Access modes */
+	unsigned char access;
+	unsigned char granularity;
+	unsigned char base_high;
 } __attribute__((packed));
 
-// GDT pointer structure
 struct gdt_ptr {
-    uint16_t limit;
+    unsigned short limit;
     uint64_t base;
 } __attribute__((packed));
 
